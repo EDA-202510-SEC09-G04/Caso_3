505d422affaf9e0ef2a45ef93c84774390371c4c
DanielGT27-Q <d.galindot@uniandes.edu.co>
2026-04-16 16:01:31 -0500

    clasificador

--- Sistema_IoT/Clasificador.java
+++ Sistema_IoT/Clasificador.java
@@ -1,3 +1,34 @@
-public class Clasificador {
+public class Clasificador extends Thread{
+
+    private Buzon buzonClasificador;
+    private boolean terminado = false;
+    private Buzon buzonServidor;
+    private int EventosFin;
+    private int numeroClasificadores;
+    private static int clasificadoresTerminados = 0;
+
+    public Clasificador( Buzon buzonClasdificador, Buzon buzonServidor,int EventosFin, int numeroClasificadores){
+
+        this.buzonClasificador = buzonClasdificador;
+        this.buzonServidor = buzonServidor;
+        this.EventosFin = EventosFin;
+        this.numeroClasificadores = numeroClasificadores;
+
+
+
+
+    }
+
+
+    @Override
+
+
+    public void run(){
+        
+    }
+
+
+
+
 
 }
